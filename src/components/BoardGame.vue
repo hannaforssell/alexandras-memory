8560954738cf0c7137c823757f6f38f70cabf6aa
Hanna Forssell <forssellhanna@gmail.com>
2023-05-30 10:57:22 +0200

    added functionlity for memory

--- src/components/BoardGame.vue
+++ src/components/BoardGame.vue
@@ -6,7 +6,13 @@ import horseImg from '/public/cards/horse.jpg';
 import lightouseImg from '/public/cards/light-house.jpg';
 import tivoliByNightImg from '/public/cards/tivoli-by-night.jpg';
 import tivoliImg from '/public/cards/tivoli.jpg';
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import lodash from 'lodash';
+
+const state = reactive({
+    update: 0,
+    victory: false
+});
 
 class BoardCard {
     constructor(
@@ -15,37 +21,83 @@ class BoardCard {
         public hasBeenRendered: number = 0,
         public flippedCard: boolean = false,
         public isShowing: boolean = false
-    ) {}
+    ) { }
 }
 
 const cards = ref<BoardCard[]>([
     new BoardCard('tivoliByNight', tivoliByNightImg),
-    new BoardCard('carousel', carouselImg),
-    new BoardCard('horse', horseImg),
-    new BoardCard('lighthouse', lightouseImg),
-    new BoardCard('tivoliByNight', tivoliByNightImg),
-    new BoardCard('tivoli', tivoliImg),
-    new BoardCard('lighthouse', lightouseImg),
-    new BoardCard('tivoli', tivoliImg),
-    new BoardCard('carousel', carouselImg),
-    new BoardCard('horse', horseImg)
+        new BoardCard('carousel', carouselImg),
+        new BoardCard('horse', horseImg),
+        new BoardCard('lighthouse', lightouseImg),
+        new BoardCard('tivoliByNight', tivoliByNightImg),
+        new BoardCard('tivoli', tivoliImg),
+        new BoardCard('lighthouse', lightouseImg),
+        new BoardCard('tivoli', tivoliImg),
+        new BoardCard('carousel', carouselImg),
+        new BoardCard('horse', horseImg)
 ]);
 
 let lastClickedCard = '';
 
+const initCards = () => {
+    const cardsToBePlaced: BoardCard[] = [
+        new BoardCard('tivoliByNight', tivoliByNightImg),
+        new BoardCard('carousel', carouselImg),
+        new BoardCard('horse', horseImg),
+        new BoardCard('lighthouse', lightouseImg),
+        new BoardCard('tivoliByNight', tivoliByNightImg),
+        new BoardCard('tivoli', tivoliImg),
+        new BoardCard('lighthouse', lightouseImg),
+        new BoardCard('tivoli', tivoliImg),
+        new BoardCard('carousel', carouselImg),
+        new BoardCard('horse', horseImg)
+    ];
+
+    const shuffledCards = lodash.shuffle(cardsToBePlaced);
+
+    cards.value = shuffledCards;
+}
+
 const handleClick = (card: BoardCard) => {
+    let counter = 0;
+
     console.log('clicked ' + card.name + ' ishowing: ' + card.isShowing, ' flipped: ' + card.flippedCard);
 
-    if (lastClickedCard == card.name) {
-        card.flippedCard = true;
-    } else {
+    if (lastClickedCard === card.name) {
+        cards.value.map((card: BoardCard) => {
+            if (card.name === lastClickedCard) {
+                card.flippedCard = true;
+            }
+        });
+
+        lastClickedCard = '';
+
+    } else if (lastClickedCard === '') {
         card.isShowing = true;
         lastClickedCard = card.name;
+    } else {
+        card.isShowing = true;
 
-        // cards.value.forEach(card => {
-        //     card.isShowing = false;
-        // })
-        
+        setTimeout(() => {
+            cards.value.forEach(card => {
+                card.isShowing = false;
+
+                lastClickedCard = '';
+                state.update++;
+            })
+        }, 3000);
+
+    }
+    state.update++;
+
+    cards.value.map((card: BoardCard) => {
+        if (card.flippedCard) {
+            counter++;
+        }
+    });
+
+    if (cards.value.length === counter) {
+        state.victory = true;
     }
 }
 
@@ -53,12 +105,11 @@ const handleClick = (card: BoardCard) => {
 
 <template>
     <main>
-        <div v-for="card in cards">
-            <GameCard @click="handleClick(card)"
-            :flipped="card.flippedCard"
-            :isShowing="card.isShowing"
-            :name="card.name" :img="card.img" />
+        <div v-for="card in cards" :key="state.update">
+            <GameCard @click="handleClick(card)" :flipped="card.flippedCard" :isShowing="card.isShowing" :name="card.name"
+                :img="card.img" />
         </div>
+        <div class="victory" v-show="state.victory">YAY! You won :)</div>
     </main>
 </template>
 
@@ -68,5 +119,10 @@ main {
     grid-template-columns: repeat(5, 1fr);
     grid-template-rows: repeat(2, 1fr);
     gap: 10px;
+}
+
+.victory {
+    font-size: 5rem;
+    text-align: center;
 }
 </style>
