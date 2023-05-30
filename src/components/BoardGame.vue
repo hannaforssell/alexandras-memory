ccfcdb60fc22dc61326844cb4213e0d1ef14df8b
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-05-30 11:07:04 +0200

    Lastfixes

--- src/components/BoardGame.vue
+++ src/components/BoardGame.vue
@@ -6,8 +6,8 @@ import horseImg from '/public/cards/horse.jpg';
 import lightouseImg from '/public/cards/light-house.jpg';
 import tivoliByNightImg from '/public/cards/tivoli-by-night.jpg';
 import tivoliImg from '/public/cards/tivoli.jpg';
-import { ref, reactive } from 'vue';
-import lodash from 'lodash';
+import { ref, reactive, onMounted } from 'vue';
+import lodash from 'lodash'; 
 
 const state = reactive({
     update: 0,
@@ -38,6 +38,10 @@ const cards = ref<BoardCard[]>([
 ]);
 
 let lastClickedCard = '';
+
+onMounted(() => {
+    initCards();
+})
 
 const initCards = () => {
     const cardsToBePlaced: BoardCard[] = [
@@ -85,7 +89,7 @@ const handleClick = (card: BoardCard) => {
                 lastClickedCard = '';
                 state.update++;
             })
-        }, 3000);
+        }, 1500);
 
     }
     state.update++;
@@ -109,8 +113,9 @@ const handleClick = (card: BoardCard) => {
             <GameCard @click="handleClick(card)" :flipped="card.flippedCard" :isShowing="card.isShowing" :name="card.name"
                 :img="card.img" />
         </div>
-        <div class="victory" v-show="state.victory">YAY! You won :)</div>
+      
     </main>
+    <div class="victory" v-show="state.victory">YAY! You won :)</div>
 </template>
 
 <style scoped>
